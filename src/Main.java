ba35a4a955ee2560706fb679084586a43dcfd0ed
Sahand <sahand.kholdi.rezaei@gmail.com>
2026-03-04 19:09:56 +0100

    loop menu until exit

--- src/Main.java
+++ src/Main.java
@@ -7,19 +7,23 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         int balance = 1000;
+        boolean isRunning = true;
 
-        System.out.println("Welcome to Banking System");
-        System.out.println("1 - Check your balance");
-        System.out.println("2 - Deposit");
-        System.out.println("3 - Withdraw");
-        System.out.println("4 - Exit");
-        System.out.print("Select an option: ");
+        while (isRunning) {
+            System.out.println("Welcome to Banking System");
+            System.out.println("1 - Check your balance");
+            System.out.println("2 - Deposit");
+            System.out.println("3 - Withdraw");
+            System.out.println("4 - Exit");
+            System.out.print("Select an option: ");
 
-        int option = scanner.nextInt();
+            int option = scanner.nextInt();
 
-        System.out.println("You selected: " + option);
+            System.out.println("You selected: " + option);
 
-
-
+            if (option == 4){
+                isRunning = false;
+            }
+        }
     }
 }
